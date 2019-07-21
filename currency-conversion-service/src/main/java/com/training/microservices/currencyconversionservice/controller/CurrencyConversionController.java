c5e1565e772c14d8cb62440f84e12b05e18bce9d
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2019-07-22 02:18:06 +0300

    using other microservice for currency calculation

--- currency-conversion-service/src/main/java/com/training/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/training/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,11 +1,15 @@
 package com.training.microservices.currencyconversionservice.controller;
 
 import com.training.microservices.currencyconversionservice.model.CurrencyConversionBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by adere on 21.07.2019.
@@ -17,8 +21,18 @@ public class CurrencyConversionController {
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean calculateValue(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
-        CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(10000L, from, to, BigDecimal.valueOf(100), quantity, quantity, 0);
+        //CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(10000L, from, to, BigDecimal.valueOf(100), quantity, quantity, 0);
 
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+
+        CurrencyConversionBean response = responseEntity.getBody();
+
+        CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(response.getId(), from, to, response.getExchangeFactor(), quantity, quantity.multiply(response.getExchangeFactor()), response.getPort());
 
         return currencyConversionBean;
     }
